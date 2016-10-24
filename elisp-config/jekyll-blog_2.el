a2391f049fff6de5a1c447408bb93bf96b4f6900
404mj <zhangshuxinmj@163.com>
2016-10-24 16:02:07 +0800

    delete unuse el file(alredy backup)

--- elisp-config/jekyll-blog_2.el
+++
@@ -1,9 +0,0 @@
-(add-to-list 'load-path "~/.emacs.d/elpa/org-jekyll-mode/") 
-(require 'org-jekyll-mode)
-(org-jekyll-mode)
-
-(setq org-jekyll/jekyll-project-root "~/404mj.github.io/")
-(setq org-jekyll/org-mode-project-root "~/404mj.github.io/_org/")
-
-(provide 'jekyll-blog)
-
